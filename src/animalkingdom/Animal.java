f9bf13160b5afe094dfcd3edc85cdf25c92163df
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-02-09 15:23:18 -0500

    create abstract animal class

---
+++ src/animalkingdom/Animal.java
@@ -0,0 +1,61 @@
+package animalkingdom;
+
+abstract class Animal 
+{
+    // shared fields
+    protected static int maxId = 0;
+    protected int id;
+    protected int food;
+    protected String name;
+    protected int year;
+
+    // constructor for all child classes
+    public Animal(String name, int year)
+    {
+        maxId ++;
+        id = maxId;
+        this.name = name;
+        this.year = year;
+    }
+
+    // abstract methods
+    abstract String getMove();
+    abstract String getBreath();
+    abstract String getReproduce();
+    
+    // shared methods implemented for all child classes
+    void eat(int amount) 
+    {
+        food += amount;
+    }
+
+    int getId()
+    {
+        return id;
+    }
+
+    String getName()
+    {
+        return name;
+    }
+
+    int getYear() 
+    {
+        return year;
+    }
+
+    void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    int getFood()
+    {
+        return food;
+    }
+
+    void setFood(int amount)
+    {
+        eat(amount);
+    }
+}
